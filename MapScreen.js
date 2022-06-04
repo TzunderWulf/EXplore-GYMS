9166f1060a0e8ad227876427f2c9f3d2f2541c01
TzunderWulf <svandenbos@tzunder.nl>
2022-06-04 21:41:33 +0200

    Added simple map on the MapScreen.

--- MapScreen.js
+++ MapScreen.js
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { View, StyleSheet, Dimensions } from 'react-native';
 
 export function MapScreen() {
     return (
-        <View>
-            <Text>Mapscreen</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map} />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
