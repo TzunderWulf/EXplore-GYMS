7009ec9b53f49b252ecab636433bc9b285613bed
TzunderWulf <svandenbos@tzunder.nl>
2022-06-04 22:31:40 +0200

    Now asks permission to see location, puts pin on user's location.

--- MapScreen.js
+++ MapScreen.js
@@ -1,24 +1,52 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { View, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { View, Text } from 'react-native';
+import { styles } from './Stylesheet';
 
 export function MapScreen() {
+    const [initialRegion, setInitialRegion] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    });
+    const [location, setLocation] = useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+    });
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location.coords);
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map} />
+            <MapView 
+                style={styles.map} 
+                region={{
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+                initialRegion={initialRegion}
+            >
+                <Marker
+                    key="End User Location"
+                    coordinate={{ latitude: location.latitude, longitude: location.longitude }}
+                    title="End User Location"
+                />
+            </MapView>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-    },
-});
